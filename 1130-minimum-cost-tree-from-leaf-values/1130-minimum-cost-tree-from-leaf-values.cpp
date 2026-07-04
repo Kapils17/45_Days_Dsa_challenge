253ceabd06bcb569f9431614c73e31a2ce1ab76f
Kapil Kumar Sharma <kapilsharma200518@gmail.com>
2026-07-04 22:20:25 +0530

    Time: 0 ms (100%), Space: 10.8 MB (78.92%) - LeetHub

---
+++ 1130-minimum-cost-tree-from-leaf-values/1130-minimum-cost-tree-from-leaf-values.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int mctFromLeafValues(vector<int>& arr) {
+
+        stack<int> st;
+        st.push(INT_MAX);
+
+        int ans=0;
+
+        for(int x:arr){
+
+            while(st.top()<=x){
+
+                int mid=st.top();
+                st.pop();
+
+                ans+=mid*min(st.top(),x);
+            }
+
+            st.push(x);
+        }
+
+        while(st.size()>2){
+
+            int x=st.top();
+            st.pop();
+
+            ans+=x*st.top();
+        }
+
+        return ans;
+    }
+};
