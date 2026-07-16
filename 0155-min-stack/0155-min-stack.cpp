c056afd3c74938052ec4b4f4d87aa419c2d1a18e
Kapil Kumar Sharma <kapilsharma200518@gmail.com>
2026-07-17 00:32:35 +0530

    Time: 56 ms (50.12%), Space: 151 MB (60.04%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,23 +1,21 @@
 class MinStack {
 public:
-
-    stack<pair<int , int >> s;
-
+   
+    stack<pair<int , int>> s;
+    
     MinStack() {
         
     }
     
     void push(int value) {
-
         if(s.empty()){
-           s.push({value , value});
-        }
+            s.push({value , value});
+        } 
 
         else{
-            int minVal = min(value , s.top().second);
-            s.push({value , minVal});
+            int minval = min(value , s.top().second);
+            s.push({value , minval});
         }
-        
     }
     
     void pop() {
