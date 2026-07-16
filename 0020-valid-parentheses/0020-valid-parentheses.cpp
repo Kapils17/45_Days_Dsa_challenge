97ada07239506ae41c4b9a8d145807260bb48bd0
Kapil Kumar Sharma <kapilsharma200518@gmail.com>
2026-07-16 23:04:22 +0530

    Time: 0 ms (100%), Space: 9 MB (18.51%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    bool isValid(string s) {
+
+        stack<char> st;
+        
+        int n = s.length();
+        
+        for(int i = 0; i < n ; i++){
+            char ch = s[i];
+            
+            if(ch == '(' || ch == '[' || ch == '{'){
+                st.push(ch);
+            }
+
+            else{
+
+            if(st.empty()){
+                return false;
+            }
+
+            if(ch == ')' && st.top() != '('){
+                return false;
+            }
+
+            if(ch == ']' && st.top() != '['){
+                return false;
+            }
+
+            if(ch == '}' && st.top() != '{'){
+                return false;
+            }
+
+            st.pop();
+
+
+
+        }
+
+
+    }
+          return st.empty();
+    }
+};
