b2cabe18afed01adae5c0090774159957b1f3507
Kapil Kumar Sharma <kapilsharma200518@gmail.com>
2026-08-03 22:50:58 +0530

    Time: 0 ms (100%), Space: 17 MB (73.81%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
@@ -1,40 +1,64 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
+             
+      vector<int> sub;
+      vector<vector<int>> ans;
 
-        vector<vector<int>> ans;
-
-        if (root == NULL) {
-            return ans;
-        }
-
-        queue<TreeNode*> q;
-        q.push(root);
-
-        while (!q.empty()) {
-
-            int n = q.size();
-            vector<int> level;
-
-            for (int i = 0; i < n; i++) {
-
-                TreeNode* curr = q.front();
-                q.pop();
-
-                level.push_back(curr->val);
-
-                if (curr->left != NULL) {
-                    q.push(curr->left);
-                }
-
-                if (curr->right != NULL) {
-                    q.push(curr->right);
-                }
-            }
-
-            ans.push_back(level);
-        }
-
+      queue<TreeNode*> q;
+      
+      if(root == NULL){
         return ans;
+      }
+      
+      q.push(root);
+      ans.push_back({root -> val});
+      q.push(NULL);
+
+      while(!q.empty()){
+        
+        TreeNode* temp = q.front();
+        q.pop();
+
+        if(temp != NULL){
+            if(temp -> left){
+                q.push(temp -> left);
+                sub.push_back(temp -> left ->val);
+            }
+            if(temp -> right){
+                q.push(temp -> right);
+                sub.push_back(temp -> right ->val);
+            }
+           }else{
+               
+               if(!sub.empty()){
+                ans.push_back(sub); 
+               }
+               
+               sub = {};
+
+
+               if(!q.empty()){
+                q.push(NULL);
+               }
+
+           }
+      }
+
+      return ans;
+
+     
+
     }
 };
