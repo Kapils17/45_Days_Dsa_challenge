749da9a0c973254916796708b3e9f12deeb813ec
Kapil Kumar Sharma <kapilsharma200518@gmail.com>
2026-08-03 21:33:59 +0530

    Time: 0 ms (100%), Space: 12.8 MB (48.61%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -9,66 +9,25 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-
-void pre1(TreeNode* p , vector<int> &ans1){
-    
-    if(p == NULL){
-
-        ans1.push_back(-1000000);
-        return;
-    }
-
-    ans1.push_back(p -> val);
-
-
-    pre1(p-> left , ans1);
-    pre1(p-> right , ans1);
-
-}
-
-void pre2(TreeNode* q , vector<int> &ans2){
- 
-  if(q == NULL){
-    ans2.push_back(-1000000);
-    return;
-  }
-
-ans2.push_back(q -> val);
-
- pre2(q -> left , ans2);
- pre2(q -> right , ans2);
-
-}
-
-
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        
-      vector<int> ans1;
-      vector<int> ans2;
+         
+        if(p == NULL && q == NULL){
+            return true;
+        }
 
-      pre1(p , ans1);
-      pre2(q , ans2);
-
-      if(ans1.size() != ans2.size()){
-        return false;
-      }
-
-      for(int i = 0; i < ans1.size(); i++){
-           
-        if(ans1[i] != ans2[i]){
+        if(p == NULL || q == NULL){
             return false;
         }
 
-      }
 
-      return true;
-      
-     
+        if(p -> val != q -> val){
+            return false;
+        }
 
-
-
+        return isSameTree(p -> left , q -> left) && isSameTree(p -> right , q -> right);
+       
 
     }
 };
