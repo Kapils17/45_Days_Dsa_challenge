6a33db0e7cbd87e3cb6c60d7a50a26095d742f77
Kapil Kumar Sharma <kapilsharma200518@gmail.com>
2026-08-17 12:09:13 +0530

    Time: 0 ms (100%), Space: 10.9 MB (66.98%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -10,20 +10,17 @@
  * };
  */
 
+
 void inorder(TreeNode* root , vector<int> &ans){
-     
-     if(root == NULL){
-        return;
-     }
+   
+   if(root == NULL){
+     return;
+   }
 
-     inorder(root -> left , ans );
+   inorder(root -> left , ans); 
+   ans.push_back(root -> val);
 
-     ans.push_back(root -> val);
-
-     inorder(root -> right , ans);
-
-
-
+   inorder(root -> right , ans);
 
 }
 
@@ -32,10 +29,8 @@ public:
     vector<int> inorderTraversal(TreeNode* root) {
         
         vector<int> ans;
-
         inorder(root , ans);
-
-        return ans;
-
+        
+         return ans;
     }
 };
