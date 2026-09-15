94f5007ca272f7e5af5fa686473ddd0c3915da12
Kapil Kumar Sharma <kapilsharma200518@gmail.com>
2026-09-15 15:08:13 +0530

    Time: 42 ms (5.36%), Space: 126.2 MB (5.18%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -11,18 +11,8 @@
  */
 class Solution {
 public:
-
-    int findposi(vector<int> ino , int n , int element){
-        for(int i = 0; i < n; i++){
-            if(ino[i] == element){
-                return i;
-            }
-        }
-
-        return -1;
-    }
     
-    TreeNode* create(vector<int> pre , vector<int> ino , int n , int &index , int start , int end){
+    TreeNode* create(vector<int> &pre , vector<int> ino , int n , int &index , int start , int end , unordered_map<int , int> &mp){
         //base case 
 
         if(index >= n || start > end){
@@ -32,10 +22,10 @@ public:
         TreeNode* root = new TreeNode(pre[index]);
         index++;
 
-        int posi = findposi(ino , n , root -> val);
+        int posi = mp[root -> val];
 
-        root -> left = create(pre , ino , n , index , start , posi - 1);
-        root -> right = create(pre , ino , n , index , posi + 1 , end);
+        root -> left = create(pre , ino , n , index , start , posi - 1 , mp);
+        root -> right = create(pre , ino , n , index , posi + 1 , end , mp);
 
         return root;
 
@@ -49,7 +39,13 @@ public:
         int instart = 0;
         int inend = n - 1;
 
-        TreeNode* root = create(preorder , inorder , n , preindex , instart , inend);
+        unordered_map<int , int> mp;
+
+        for(int i = 0; i < n; i++){
+            mp[inorder[i]] = i;
+        }
+
+        TreeNode* root = create(preorder , inorder , n , preindex , instart , inend , mp);
         return root;
         
     }
