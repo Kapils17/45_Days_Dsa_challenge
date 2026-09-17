07c32e2aaead88ebcbcb92ffe350c49f1963d95c
Kapil Kumar Sharma <kapilsharma200518@gmail.com>
2026-09-17 19:04:29 +0530

    Time: 0 ms (100%), Space: 10.2 MB (70.12%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -4,16 +4,16 @@ public:
         int row = matrix.size();
         int col = matrix[0].size();
 
-        for(int i = 0 ; i < row ; i++){
-            for(int j = i; j < col ; j++){
+        for(int i = 0; i < row ; i++){
+            for(int j = i; j < col; j++){
                 swap(matrix[i][j] , matrix[j][i]);
             }
         }
 
-        for(int i = 0; i < row ; i++){
-        reverse(matrix[i].begin(), matrix[i].end());
+        for(int i = 0; i < row; i++){
+            reverse(matrix[i].begin() , matrix[i].end());
         }
 
-
+       
     }
 };
