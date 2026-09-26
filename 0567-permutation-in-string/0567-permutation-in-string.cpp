6c990495109db8516d3841c7f585956315f756f1
Kapil Kumar Sharma <kapilsharma200518@gmail.com>
2026-09-26 17:43:38 +0530

    Time: 0 ms (100%), Space: 10 MB (46.08%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,61 +1,51 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-         
-    int size1 = s1.length();
-    int size2 = s2.length();
+        
+        int size1 = s1.size();
+        int size2 = s2.size();
 
-    if(size1 > size2){
+        if(size1 > size2){
+            return false;
+        }
+
+        vector<int> freq(26 , 0);
+
+        for(int i = 0; i < size1; i++){
+            freq[s1[i] - 'a']++;
+        }
+
+        int low = 0;
+        int high = size1 - 1;
+
+        vector<int> freq2(26 , 0);
+
+        for(int i = low ; i <= high; i++){
+           freq2[s2[i] - 'a']++;
+        }
+
+        if(freq == freq2){
+            return true;
+        }
+        
+        while(high + 1 < size2){
+            low ++;
+            high ++;
+
+            freq2[s2[low - 1] - 'a']--;
+            freq2[s2[high] - 'a']++;
+
+            if(freq == freq2){
+                return true;
+            }
+        }
+
         return false;
-    }
-
-    vector<int> v(26,0);
-
-    for(int i = 0 ; i < size1; i++ ){
-        v[s1[i] - 'a']++;
-    } 
 
 
 
-    int low = 0;
-    int high = size1 - 1;
 
-    vector<int> v2(26,0);
 
-    for(int i = low ; i <= high ; i++){
-        v2[s2[i] - 'a']++;
 
     }
-
-    
-  if(v == v2){
-     return true;
-       }
-
-    while(high < size2 - 1){
-      
-       low ++;
-       high ++;
-
-
-
-       v2[s2[low - 1] - 'a']--;
-       v2[s2[high ]-'a']++;
-
-       
-        if(v == v2){
-        return true;
-       }
-
-      
-    }
-      
-
-   return false;
-
-    }
-
-    
-
-    
 };
